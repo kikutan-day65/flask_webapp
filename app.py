82099a0dce3709184448fde928563975fa41c218
Haruto Mori <baystarsman@outlook.jp>
2022-05-23 00:32:13 +0200

    add database file and edit app.py

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
+from email.policy import default
+from turtle import title
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String(300), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
 
 @app.route("/")
 def index():
     return render_template("index.html")
-    
